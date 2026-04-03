a77113b7c2141a31cc56a16a46c92e5faa91908c
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-03 18:14:13 +0530

    py.c - Tested MLang

--- debug/py.c
+++ debug/py.c
@@ -1,14 +1,9 @@
-#include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    Py_Initialize();
-    if (Py_IsInitialized())
-    {
-        PyRun_SimpleString("print('Hello from embedded Python 3.14!')");
-    }
-    Py_Finalize();
-    printf("Python embedding test completed successfully.\n");
+    system("cd ../console && python MLang.py");
+
     return 0;
 }
