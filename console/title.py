65691f8a3df85766a8994b7d725f42e6b392fbfc
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-07 11:36:12 +0530

    title.py - Title screen

--- console/title.py
+++ console/title.py
@@ -4,6 +4,7 @@ def title() :
     print("\n-----------------------------------------------------------\n");
     print("\n----MACHINE LEARNING PIPEPLINE AUTOMATOR", VER_STRT, "(MLAuto)----\n");
     print("\n------Copyright (c) Akshay_babu, All rights reserved!------\n");
+    print("\n---------------------MLang Console-------------------------\n");
     print("\n-----------------------------------------------------------\n");
     
 if __name__ == "__main__" :
